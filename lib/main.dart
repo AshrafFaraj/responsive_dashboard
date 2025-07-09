6e1a773c711842a6c26ed8670cffc073efebc4d3
Ashraf <farage969@gmail.com>
2025-07-09 11:28:47 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const Adaptive_layout(),
+    );
+  }
+}
+
+class Adaptive_layout extends StatelessWidget {
+  const Adaptive_layout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
